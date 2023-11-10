e2016fe79e384b3ca5cb1b619595ee1d2f72f23a
manhngo-dn <ngomanh.fot.hou@gmail.com>
2023-11-11 01:41:19 +0700

    update

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: "Udagram",
-  apiHost: "http://udagram-env.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagram717432590967.s3-website-us-east-1.amazonaws.com/",
 };
 
 /*
